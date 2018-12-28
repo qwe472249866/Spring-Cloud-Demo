7b597dda182767c8155d1ef387ce19119db0f934
qwe472249866 <929515737@qq.com>
2018-12-28 16:43:41 +0800

    gateway

---
+++ gateway/src/main/java/com/zp/demo/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.zp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
+
